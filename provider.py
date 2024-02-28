815a969248bc59c1c95ccda24eb3bd5ad72decf3
AnimaLibera <animalibera@mail.de>
2024-02-28 19:23:46 +0100

    Load JSON Currency Pair Metainformations

--- provider.py
+++ provider.py
@@ -1,5 +1,6 @@
 from pandas_datareader import data as pdr
 import yfinance as yf
+import json
 
 yf.pdr_override()
 
@@ -16,3 +17,12 @@ def get_major_forex_returns(start="2003-01-01", end="2023-12-31", interval="1wk"
     data = pdr.get_data_yahoo(majors, start=start, end=end, interval=interval)["Close"].pct_change().dropna()
     data.columns = column_names
     return data
+
+def get_currency_pairs_metainformation(relativ_path = "./", file_name = "pairs.json"):
+    """Load local JSON File with Currency Pairs Metainformation"""
+    with open(relativ_path + file_name) as file:
+        return json.load(file)
+    
+def make_pairs_list_from_metainformation(list_of_pairs_metainformation):
+    """Make List of Currency Pairs from List of Metainformation about Currency Pairs for Investing.com"""
+    return [pair["basis"]["currency"]["code"]["iso"] + pair["delimiter"]["investingdotcom"] + pair["counter"]["currency"]["code"]["iso"] for pair in list_of_pairs_metainformation]
